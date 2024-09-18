beaa8db65392f2bf19b292d0019310ea27a5a100
Dhanashree2023 <Prachi123451@gmail.com>
2024-09-18 08:39:13 +0530

    jhkj

--- src/com/Test.java
+++ src/com/Test.java
@@ -4,6 +4,12 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("TEST");
+		
+		String s = "Java";
+		System.out.println("String is="+s);
+		
+		int i = 10;
+		
 	}
 
 }
